d064018cad7b574776d9b8776fc12e47f59715b8
chingzhangliu <43977867+chingzhangliu@users.noreply.github.com>
2018-12-26 10:33:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,18 @@ SECRET= os.environ['CHANNEL_SECRET']
 line_bot_api=LineBotApi(ACCESS_TOKEN)
 handler=WebhookHandler(SECRET)
 
+rich_menu_to_create = RichMenu(
+    size=RichMenuSize(width=2500, height=843),
+    selected=False,
+    name="Nice richmenu",
+    chat_bar_text="Tap here",
+    areas=[RichMenuArea(
+        bounds=RichMenuBounds(x=0, y=0, width=2500, height=843),
+        action=URIAction(label='Go to line.me', uri='https://line.me'))]
+)
+rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
+print(rich_menu_id)
+
 @app.route("/")
 def hello_world():
   return "hello world!"
