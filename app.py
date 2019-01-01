d6a6c47ac35e6d4e731a74d9c5be923371b8f17e
chingzhangliu <43977867+chingzhangliu@users.noreply.github.com>
2019-01-01 21:49:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def handle_message(event):
       result_message = r[0] + "("+r[1]+")"
       line_bot_api.push_message(event.source.user_id, TextSendMessage(text=result_message))
     profile = line_bot_api.get_profile(addr)
-    line_bot_api.push_message(event.source.user_id, TextSendMessage(text=profile.display_name))
+    line_bot_api.push_message(event.source.user_id, TextSendMessage(text='123'))
     
 def prk(num):
   result = []
