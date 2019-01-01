aee570eee134380b4313320089db2aa2b788d106
chingzhangliu <43977867+chingzhangliu@users.noreply.github.com>
2019-01-01 20:23:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,8 @@ def handle_message(event):
   else:
     reply =TextSendMessage(text="您所搜尋的結果為：\n")
     line_bot_api.reply_message(event.reply_token,reply)
-    for r in prk():
+    rank=prk()
+    for r in rank:
       result_message = r[0] + "("+r[1]+")"
       line_bot_api.push_message(event.source.user_id, TextSendMessage(text=result_message))
     
