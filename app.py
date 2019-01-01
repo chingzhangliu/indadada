c189f6a3ced898b074e5cb6d93d7c9a3e7d33ef2
chingzhangliu <43977867+chingzhangliu@users.noreply.github.com>
2019-01-01 20:17:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,6 +47,28 @@ def handle_message(event):
   if '!h' in _token[0]:
     _message=TextSendMessage(text="1")
     line_bot_api.reply_message(event.reply_token, _message)
+  else:
+    reply =TextSendMessage(text="您所搜尋的結果為：\n")
+    line_bot_api.reply_message(event.reply_token,reply)
+    for r in prk():
+      result_message = r[0] + "("+r[1]+")"
+      line_bot_api.push_message(event.source.user_id, TextSendMessage(text=result_message))
+    
+def prk():
+  result = []
+  target = 'https://www.pixiv.net/ranking.php?mode=female'
+  r = requests.get(target)
+  soup=BeautifulSoup(r.text,'html.parser')
+  res=soup.find('div',{'class':'ranking-items adjust'})
+  res_rk=res.find_all('section',{'class':"ranking-item"})
+  for idx, rk in enumerate(res_rk):
+    if idx < 2:
+      tag=rk.find('h2').find('a')
+      title=tag.get_text()
+      href=tag['href']
+      result.append((title,href))
+  return result
+  
 import os
 if __name__=="__main__":
   
