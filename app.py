269092e9d9fd62a9e7669ea3522f8d1ef591f3b5
chingzhangliu <43977867+chingzhangliu@users.noreply.github.com>
2019-01-01 21:54:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,23 +45,19 @@ def handle_message(event):
   _token = msg.strip().split(" ")
   _low_token = _token[0].lower()
   if '!h' in _token[0]:
-    _message=TextSendMessage(text="給個數字")
+    _message=TextSendMessage(text="給個數字?")
     line_bot_api.reply_message(event.reply_token, _message)
-    reply =TextSendMessage(text="您所搜尋的結果為：\n")
-    line_bot_api.reply_message(event.reply_token,reply)
   else:
     
     addr=event.reply_token
     
-    reply =TextSendMessage(text='111')
+    reply =TextSendMessage(text='111?')
     line_bot_api.reply_message(event.reply_token,reply)
     
     rank=prk(int(_token[0]))
     for r in rank:
       result_message = r[0] + "("+r[1]+")"
       line_bot_api.push_message(event.source.user_id, TextSendMessage(text=result_message))
-    profile = line_bot_api.get_profile(addr)
-    line_bot_api.push_message(event.source.user_id, TextSendMessage(text='123'))
     
 def prk(num):
   result = []
