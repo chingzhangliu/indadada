71b9110f25d2693f2771ff51cd9a7585dd5b215c
chingzhangliu <43977867+chingzhangliu@users.noreply.github.com>
2019-01-02 14:46:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,9 @@ def handle_message(event):
   if '!h' in _token[0]:
     _message=TextSendMessage(text="我要喝飲料")
     line_bot_api.reply_message(event.reply_token, _message)
+  elif 'send' in _token(0):
+    idtxt=event.source.user_id
+    lin_bot_api.push_message('U37a3bf674521ba20444e4beb1ded3f27',TextSendMessage(text=idtxt))
   elif '茶湯會' in _token[0]:
     img=ImageSendMessage(
     original_content_url='https://twcoupon.com/images/menu/p_teapatea_2017_5_n.jpg',
